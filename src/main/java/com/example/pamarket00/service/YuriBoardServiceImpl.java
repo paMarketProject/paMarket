416899d30ad117bccf70e734d0fd8aa547d21ffb
YOUNGMAN\dudal <dudals334@naver.com>
2022-12-14 17:26:21 +0900

    Merge branch 'master' of https://github.com/paMarketProject/paMarket

---
+++ src/main/java/com/example/pamarket00/service/YuriBoardServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.pamarket00.service;
+
+import com.example.pamarket00.common.FileUtils;
+import com.example.pamarket00.dto.BoardDto;
+import com.example.pamarket00.dto.FileDto;
+import com.example.pamarket00.mapper.YuriBoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.List;
+
+@Service
+public class YuriBoardServiceImpl implements YuriBoardService{
+    @Autowired
+    private YuriBoardMapper yuriBoardMapper;
+    @Autowired
+    private FileUtils fileUtils;
+
+    @Override
+    public List<BoardDto> selectProductBoardList() throws Exception{
+        List<BoardDto> list = null;
+        list = yuriBoardMapper.selectProductBoardList();
+        return list;
+    }
+
+//    게시물저장
+    @Override
+    public void insertProductBoard (BoardDto board, MultipartHttpServletRequest uploadFiles) throws Exception{
+        yuriBoardMapper.insertProductBoard(board);
+
+        List<FileDto> fileList = fileUtils.parseFileInfo(board.getBoardNum(),uploadFiles);
+//uploadFiles였는데
+
+        if(CollectionUtils.isEmpty(fileList) == false){
+            yuriBoardMapper.insertProductBoardFileList(fileList);
+        }
+    }
+
+    @Override
+    public void updateProductBoard(BoardDto board) throws Exception {
+        yuriBoardMapper.updateProductBoard(board);
+    }
+
+    @Override
+    public void deleteProductBoard(int boardNum) throws Exception {
+        yuriBoardMapper.deleteProductBoard(boardNum);
+    }
+
+    @Override
+    public FileDto selectProductBoardFileInfo(int fileNum, int boardNum) throws Exception {
+        return yuriBoardMapper.selectProductBoardFileInfo(fileNum,boardNum);
+    }
+}
