96f8006870f98ac7f1a48329408b95ef887f2cf6
hsseob <foreverkhs@gmail.com>
2022-12-22 10:09:44 +0900

    dd

--- src/main/java/com/example/pamarket00/dto/Message.java
+++ src/main/java/com/example/pamarket00/dto/Message.java
@@ -4,10 +4,12 @@ public class Message {
     private String messageContent;
 
     public String getMessageContent() {
+
         return messageContent;
     }
 
     public void setMessageContent(String messageContent) {
+
         this.messageContent = messageContent;
     }
 }
